0df0197a7dd74f7e0f77ba0bf6e89275d715a63e
FilipsCalijs <filips.calijs@gmail.com>
2024-09-22 21:46:32 +0300

    add q&a and script

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
