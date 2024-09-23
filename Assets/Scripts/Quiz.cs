45558d3322dd6e22f5632eb53ed2a979854219a9
FilipsCalijs <filips.calijs@gmail.com>
2024-09-23 09:34:14 +0300

    connect text to answer buttons

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Quiz : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons; 
 
     void Start()
     {
-        
+        questionText.text = question.GetQuestion();
     }
 
-    void Update()
-    {
-        
-    }
 }
